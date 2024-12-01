19d1a8c3acddf038d54e7dbe9c1097b9a43f72f6
Philipp Hofmann <phofmann200@gmail.com>
2024-12-01 19:52:41 +0100

    changed fgets to my own get next line

--- day_1/main.c
+++ day_1/main.c
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error: Missing input.\n");    
         return (1);   
     }
-    FILE *file = fopen(argv[1], "r");
-    char line[256];
-
     t_list *left = NULL;
     t_list *right = NULL;
 
-    while(fgets(line, sizeof(line), file))
+    int fd = open(argv[1], O_RDONLY);
+    char *line;
+    
+    while((line = get_next_line(fd)) != NULL)
     {
        char **values = ft_split(line, ' ');
        t_list *node = ft_lstnew(create_num(ft_atoi(values[0])));
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     right = mergeSort(right);
     printf("total distance : %d\n", calc_total_distance(left, right));
     printf("simalarity score : %d \n", calc_similarity_score(left, right));
-    fclose(file);
+    close(fd);
     ft_lstclear(&left, &free);
     ft_lstclear(&right, &free);
     return (0);
